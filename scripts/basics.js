292b7d28df016cd8cce240b526383eab087c3fd4
Angie <35319938+Coates-Angie@users.noreply.github.com>
2019-11-24 00:54:45 -0700

    Add files via upload

---
+++ scripts/basics.js
@@ -0,0 +1,74 @@
+// This shows my understanding of functions,variables, parameters, and conditional statements
+function computeBMI() {
+    // Select the Elements
+    var height = Number(document.getElementById("height").value);
+    var heightunits = document.getElementById("heightunits").value;
+    var weight = Number(document.getElementById("weight").value);
+    var weightunits = document.getElementById("weightunits").value;
+
+
+    //This will convert the units to metrics in order to calculate inches and pounds
+    if (heightunits === "inches") {
+        height /= 39.37007874;
+    }
+
+    if (weightunits === "lb") {
+        weight /= 2.20462;
+    }
+
+    //BMI calculation and rounded
+    var BMI = Math.round(weight / Math.pow(height, 2));
+
+    //Display result of calculation
+    document.getElementById("bmiOutput").innerText = BMI;
+
+    //This will give the user a response to what the BMI number means
+    var bmiOutput = Math.round(BMI * 10) / 10;
+    if (bmiOutput < 18.5) {
+        document.getElementById("response").innerText = "Underweight";
+    } else if (bmiOutput >= 18.5 && bmiOutput <= 25) {
+        document.getElementById("response").innerText = "Normal";
+    } else if (bmiOutput >= 25 && bmiOutput <= 30) {
+        document.getElementById("response").innerText = "Overweight";
+    } else if (bmiOutput > 30) {
+        document.getElementById("response").innerText = "Obese";
+    }
+}
+
+//This is the AJAX section
+
+// Our JSON data
+function listBoys() {
+    var data = {
+        "myBoys": [{
+                "childName": "Porter Coates",
+                "born": "2002"
+            },
+            {
+                "childName": "Lukas Coates",
+                "born": "2004"
+            },
+            {
+                "childName": "Dane Coates",
+                "born": "2011"
+            }
+        ]
+    }
+
+    // Put the data into a variable and format with HTML tags
+    var output = "<h1>My Boys</h1>";
+    output += "<ul>";
+
+    // Loop through the Boys
+    for (var i in data.myBoys) {
+        output += "<li>" + data.myBoys[i].childName + " (Born: " + data.myBoys[i].born + ")</li>";
+    }
+
+    output += "</ul>";
+
+    // Output the data to the "sonsList" element
+    document.getElementById("sonsList").innerHTML = output;
+}
+
+// Load the above function when the window loads
+window.onload = listBoys;
